b30955d5d06473b8101d9c40274ac22d3fd9e636
Danny <65949272+DannyMannn@users.noreply.github.com>
2022-02-11 11:00:30 -0600

    Added requirements file and changed time zone to Mexico
    Please write all the dependencies in the requirements file.

--- plank/settings.py
+++ plank/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'America/Mexico_City'
 
 TIME_ZONE = 'UTC'
 
