efecbb9d1a3721a2673b6e9b57ec12690808c2a2
Mokkuman <droseher@gmail.com>
2022-02-26 10:54:50 -0600

    Añadiendo vista index

--- core/views.py
+++ core/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse #prueba, first commit
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hola esto plank OwO")
+    return render(request,"core/home.html")
