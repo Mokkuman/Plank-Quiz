0969f87762c707df347ed92f052385a586fba6c5
Mokkuman <droseher@gmail.com>
2022-03-26 21:23:25 -0600

    Media folder añadido, habilitada la imagen por defecto en el usuario y agregando home

--- plank/urls.py
+++ plank/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path('',include("usuario.urls")),
 ]
 #Para el modo de produccion aka debug=true
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- usuario/models.py
+++ usuario/models.py
@@ -10,7 +10,7 @@ class User(AbstractBaseUser,PermissionsMixin):
     apellido = models.CharField(max_length=100, blank=True)
     email = models.EmailField(max_length=255, unique= True)
     password = models.CharField(max_length=200)
-    #imagenPerfil = models.ImageField(...)
+    imagenPerfil = models.ImageField(upload_to='pfp',default="planky.png",null=True,blank=True)
     
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
